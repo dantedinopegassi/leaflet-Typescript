526fa9c106f7883620c82021bc7be9a467a30197
dinopegassi <nmbost@gmail.com>
2024-06-18 23:06:59 +0930

    aniadido popup con ubicacion dinamica

--- proyectos/01/02-ts-first-map/index.ts
+++ proyectos/01/02-ts-first-map/index.ts
@@ -1,18 +1,31 @@
-import { TileLayer, Map } from "leaflet";
-import { tileOptionsMaxZoomAttribution } from "./options"
+import { Map, Popup } from "leaflet";
+import { tile, pop } from "./options";
 
 const mymap = new Map("map").setView(
-  [-32.03079481720597, -60.30631542205811],
+  [-31.598876363018576, -60.70358276367188],
   14
 );
 
-const tile1: TileLayer = new TileLayer(
-  "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
-  tileOptionsMaxZoomAttribution
-);
+const tile1 = tile();
+
+const pop1 = pop(mymap, [-31.598876363018576, -60.70358276367188]);
 
 tile1.addTo(mymap);
+pop1.addTo(mymap);
 
 mymap.on("click", (e) => {
-  alert(e.latlng.lat + "\n" + e.latlng.lng);
+  // alert(e.latlng.lat + "\n" + e.latlng.lng);
+  console.log("huepa", e.latlng);
+  pop1
+    .setLatLng(e.latlng)
+    .setContent(`<p>ubicacion:</br>` + e.latlng.toString() + `</p>`);
+  toggle(mymap, pop1);
 });
+
+function toggle(map: Map, popup: Popup) {
+  if (map.hasLayer(popup)) {
+    map.closePopup(popup);
+  } else {
+    map.openPopup(popup);
+  }
+}
--- proyectos/01/02-ts-first-map/options.ts
+++ proyectos/01/02-ts-first-map/options.ts
@@ -1,7 +1,20 @@
-import { TileLayerOptions } from "leaflet";
+import { LatLngExpression, Map, Popup, TileLayer, TileLayerOptions } from "leaflet";
 
-export const tileOptionsMaxZoomAttribution: TileLayerOptions = {
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    maxZoom: 19,
-  };
+const tileOptionsMaxZoomAttribution: TileLayerOptions = {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  maxZoom: 19,
+};
+
+export const tile = () => {
+  return new TileLayer(
+    "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+    tileOptionsMaxZoomAttribution
+  );
+};
+
+export const pop = (map: Map, latlng: LatLngExpression) => {
+  return new Popup(latlng, {
+    content: "<p>Hello world!<br />This is a nice popup.</p>",
+  }).openOn(map);
+};
